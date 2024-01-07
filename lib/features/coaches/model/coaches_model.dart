39b2c229d0c89f2040a798bfacf49c1b601cae0c
Ismaeil <ismaeil.alaranji@gmail.com>
2024-01-07 16:57:38 +0300

    first commit with nour2

--- lib/features/coaches/model/coaches_model.dart
+++ lib/features/coaches/model/coaches_model.dart
@@ -6,6 +6,4 @@ final double rate;
 final bool isSelected;
 
   CoachesModel({required this.name, required this.description, required this.imgUrl, required this.rate,required this.isSelected});
-
-
 }
